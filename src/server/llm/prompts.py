bd02193204edc95126e2d1e3ecd485decef0b028
Equipo45 <ismiruiz@gmail.com>
2024-04-12 15:12:35 +0200

    change pdm folder

--- src/server/llm/prompts.py
+++ src/server/llm/prompts.py
@@ -13,5 +13,10 @@ prompts = {
     {articles}
     Formula una pregunta complicada, sin mencionar el artículo, con {options} respuestas correctas. Devuelve finalmente la letra de la repuesta correcta.
     {format_instructions}
+    """,
+    1.3: """Eres un examinador de oposiciones español. Vas a recibir un articulo relativo a una oposición y deberas hacer {options} preguntas tipo test, con las opciones A), B), C) y una sola opcion correcta.
+    {articles}
+    Tu objetivo es hacer una pregunta dificil de resolver, con respuesta que sean muy parecidas entre si, sin mencionar el artículo. Devuelve finalmente la letra de la repuesta correcta.
+    {format_instructions}
     """
 }
